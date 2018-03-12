297c8170fe74323183ca7d88b8de9f21e04fd663
pmistry9597 <pmistry9597@gmail.com>
2018-03-12 11:08:28 -0400

    Added connection.h for connection class declaration (still incomplete).

---
+++ connection.h
@@ -0,0 +1,44 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#include <queue>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <string>
+
+// winsock stuff
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <mwsock.h>
+// end of winsock stuff
+
+class Connection {
+	SOCKET connection; // socket handle
+	std::queue<std::string> sendQueue; // queue of messages to be sent
+	std::mutex sendMutex; // mutex to protect sendQueue
+	std::queue<std::string> recvQueue; // queue of received messages
+	std::mutex recvMutex; // mutex to protect recvQueue
+	std::condition_variable recv_cv; // condition variable to wait for incoming message
+
+	// wsaoverlapped for recv
+	WSAOVERLAPPED recvOverlapped;
+	WSAOVERLAPPED sendOverlapped; // wsaoverlapped for send
+	std::atomic<bool> quit; // true if the worker thread should quit
+
+	// wsabuf for receving msgs
+	WSABUF recvBuf;
+	// function that will run whenever a message is received
+	std::function<void()> recvEvent;
+	// ip and port stored here
+	std::string str_ip_address; int int_port;
+	// function that will be run in seperate thread to send messages in the sendQueue
+	std::condition_variable send_cv; // to wakeup sendWorker when sendQueue is not empty
+	void sendWorker();
+	void recvWorker();
+public:
+	// add more stuff here
+};
+
+#endif
